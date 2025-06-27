18f87277d28b2d534bc2feb65603e20f31f7afa3
lizahorokh <horok666@mit.edu>
2025-06-26 19:00:43 -0700

    Working copy

--- server_setup/src/main.rs
+++ server_setup/src/main.rs
@@ -1,15 +1,43 @@
-use axum::{Router, routing::{get, post}, extract::{State, Json}};
+use axum::{Router, routing::{get, post}, extract::{State, Json}, debug_handler};
 use serde::{Serialize, Deserialize};
+use anyhow::{anyhow};
 use std::{sync::{Arc, Mutex}, net::SocketAddr};
 use serde_json;
 use tokio::net::TcpListener;
 use lettre::message::{header, Message};
 use lettre::{SmtpTransport, Transport, transport::smtp::authentication::Credentials};
 use rusqlite::{params, Connection, Result};
-use fetch_data_lib :: {create_pb_signals};
-use verify_proof_lib :: {verify_proof};
+use fetch_data_lib :: {get_set_of_all_users};
+use verify_proof_lib :: {verify_pod};
 use database_lib::{Email, create_table, insert_email_to_database, get_email_from_database, list_all_emails_in_database};
 use chrono::prelude::*;
+use pod2::{self,
+    middleware::{
+        VDSet,
+        Params,
+        Pod,
+        PodId,
+        Hash,
+        RecursivePod,
+        Value,
+        containers::Set,
+        RawValue,
+        KEY_SIGNER,
+        CustomPredicateRef, PodType, Predicate, Statement,
+        StatementArg, TypedValue, KEY_TYPE, Operation
+    },
+    backends::plonky2::{
+        basetypes::{C, D, F},
+    },
+    frontend::{
+        MainPodBuilder,
+        MainPod
+    },
+    backends::plonky2::mainpod,
+    timed,
+    op
+};
+
 
 #[derive(Debug, Deserialize, Clone, Serialize, PartialEq)]
 pub struct EmailReceived{
@@ -33,39 +61,61 @@ async fn create_the_message(list_senders: Vec<String>, message : String) -> Stri
     result = message + "\nBest, \nParticipant of a group : \n";
     for sender in list_senders{
         result = result + &sender + "\n";
-    }    result 
+    }
+    result 
 }
 
-async fn receive_email(State(database_conn): State<EmailDatabase>, Json(email): Json<EmailReceived>) -> String{
-    let date: String= Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+async fn parse_pod(pod: MainPod) -> anyhow::Result<String>{
     
-    let email_database = Email{to: email.to.clone(), header: email.header.clone(), message: email.message.clone(), senders: email.senders.clone(), group_signature: email.group_signature.clone(), date: date.clone()};
+    let message:String = match pod.get("message"){
+        Some(body) => match body.typed() {
+            TypedValue::String(s) => s.to_string(),
+            _ => return Err(anyhow!("Sorry, could not find the message.")),
+        },
+        None => return Err(anyhow!("Sorry, could not find the message.")),
+    };
+    Ok(message)
+}
 
-    let to_addr  = email.to.clone().unwrap_or_else(|| "sansome-talk@0xparc.org".into());
-    let subject   = email.header.clone();      // or borrow &email.header
-    let pb_signals = match create_pb_signals(email.senders.clone(), &email.message.clone()).await {
-        Ok(body)  => body,
-        Err(err) => return format!("Sorry, could not process your request. \n {err}"),
-    };
-    let mut text = create_the_message(email.senders.clone(), email.message.clone()).await;
-    let input_pb_signals = match serde_json::to_string(&pb_signals){
+#[debug_handler]
+async fn receive_email(State(database_conn): State<EmailDatabase>, email_str : String) -> String{
+    let email : MainPod = match serde_json::from_str(&email_str.clone()) {
         Ok(body) => body,
-        Err(err) => return format!("Error processing public signals. Check the input fomating."),
+        Err(err) => return format!("Sorry, could not parse the pod due to {err}."),
     };
-    let flag = verify_proof(&email.group_signature, &input_pb_signals,  &"../verification_key.json".to_string()).await;
-   
+    println!("Here, got pod!");
+    let date: String= Utc::now().format("%Y-%m-%d %H:%M:%S").to_string();
+    let to_addr = "sansome-talk@0xparc.org";
+    let subject   = "Kudos!";      // or borrow &email.header
+    let (list_of_senders, pb_signals) = match get_set_of_all_users(email.clone()).await {
+        Ok(body) => body,
+        Err(err) => return format!("Sorry, could not parse the pod due to {err}."),
+    };
+
+    let flag = verify_pod(email.clone(), pb_signals.clone()).await;
+    println!("Verified proof here!");
+    let message = match parse_pod(email.clone()).await{
+        Ok(body) => body,
+        Err(err) => return format!("{err}"),
+    };
+    println!("Got the message here!");
+    let email_database = Email{to:Some("sansome-talk@0xparc.org".to_string()), header: "Kudos!".into(), message: message.clone(), senders: list_of_senders.clone(), group_signature: email_str.clone(), date: date.clone()};
+    let mut text = create_the_message(list_of_senders.clone(), message.clone()).await;
+    println!("Got text: \n {text}");
     match flag {
         Ok(body) => { 
             if body {
                 let email_id = insert_email_to_database(&database_conn.lock().unwrap(), &email_database).unwrap();
-                
+                println!("Sending message now...");
                 let letter = Message::builder()
                                     .from("kudos@0xparc.org".parse().unwrap())
                                     .to(to_addr.parse().unwrap())
                                     .subject(subject)
                                     .header(header::ContentType::TEXT_PLAIN)
-                                    .body(text + &format!("\n \n Date: {} \n Email id: {} \n \n Group Signature: {} \n (Trust us bro)", date, email_id, &email.group_signature))
+                                    .body(text + &format!("\n \n Date: {} \n Email id: {} \n \n Group Signature: {} \n(Trust us)", date, email_id, email_str.clone()))
                                     .unwrap();
+                                    
+                println!("Created the message...");
                 let creds = Credentials::new("kudos@0xparc.org".into(), "szmi aljp ugko evld".into());
                 let mailer = SmtpTransport::relay("smtp.gmail.com").unwrap().credentials(creds).build();
                 match mailer.send(&letter){
